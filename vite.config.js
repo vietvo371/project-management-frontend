e9167866a2ce044a29b63e4faa00a0033ff26752
Văn Việt <vietvo371@gmail.com>
2025-06-04 12:16:59 +0700

    Add .env to .gitignore, update dependencies, and configure server proxy in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -10,6 +10,11 @@ export default defineConfig({
     vue(),
     vueDevTools(),
   ],
+  server: {
+    proxy: {
+      '/api': 'http://localhost:8000'
+    }
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
